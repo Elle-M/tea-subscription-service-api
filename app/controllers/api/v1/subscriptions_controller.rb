ed04d040dd2e02dcd423103ed5961bade1fa3dab
Elle Majors <hellolovely@ellemajors.com>
2023-06-01 10:58:11 -0600

    add: update to only frequency and status, and test

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -7,16 +7,24 @@ class Api::V1::SubscriptionsController < ApplicationController
   end
 
   def create
-    @subscription = Subscription.new(subscription_params)
-    if @subscription.save
-      render json: @subscription, status: :created
+    @customer = Customer.find(params[:customer_id])
+    @subscription = @customer.subscriptions.create!(subscription_params)
+    render json: @subscription, status: :created
+  end
+
+  def update
+    @customer = Customer.find(params[:customer_id])
+    @subscription = @customer.subscriptions.find(params[:id])
+    if @subscription.update(subscription_update_params)
+      render json: @subscription
     else
       render json: @subscription.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @subscription = Subscription.find(params[:id])
+    @customer = Customer.find(params[:customer_id])
+    @subscription = @customer.subscriptions.find(params[:id])
     @subscription.destroy
   end
 
@@ -24,5 +32,12 @@ class Api::V1::SubscriptionsController < ApplicationController
 
   def subscription_params
     params.permit(:title, :price, :status, :frequency, :customer_id, :tea_id)
+  end
+
+  def subscription_update_params
+    params.permit(:status, :frequency).tap do |whitelisted|
+      whitelisted.delete(:status) if params[:status].nil?
+      whitelisted.delete(:frequency) if params[:frequency].nil?
+    end
   end
 end
--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -23,7 +23,7 @@ describe "Customers Subscriptions API" do
 
   it "can get all subscriptions for a customer" do
     subscription_1 = Subscription.create!(title: "Subscription 1", price: 5.00, status: "active", frequency: 1, customer_id: @customer.id, tea_id: @tea.id)
-    subscription_2 = Subscription.create!(title: "Subscription 2", price: 5.00, status: "active", frequency: 1, customer_id: @customer.id, tea_id: @tea.id)
+    subscription_2 = Subscription.create!(title: "Subscription 1", price: 5.00, status: "active", frequency: 2, customer_id: @customer.id, tea_id: @tea.id)
 
     get "/api/v1/customers/#{@customer.id}/subscriptions"
 
@@ -36,12 +36,60 @@ describe "Customers Subscriptions API" do
     expect(parsed[0][:frequency]).to eq("1")
     expect(parsed[0][:customer_id]).to eq(@customer.id)
     expect(parsed[0][:tea_id]).to eq(@tea.id)
-    expect(parsed[1][:title]).to eq("Subscription 2")
+    expect(parsed[1][:title]).to eq("Subscription 1")
     expect(parsed[1][:price]).to eq(5.00)
     expect(parsed[1][:status]).to eq("active")
-    expect(parsed[1][:frequency]).to eq("1")
+    expect(parsed[1][:frequency]).to eq("2")
     expect(parsed[1][:customer_id]).to eq(@customer.id)
     expect(parsed[1][:tea_id]).to eq(@tea.id)
+  end
+
+  it "can update the frequency of subscription for a customer" do
+    subscription_1 = Subscription.create!(title: "Subscription 1", price: 5.00, status: "active", frequency: 1, customer_id: @customer.id, tea_id: @tea.id)
+    subscription_params = { title: "Subscription 1", price: 5.00, status: "active", frequency: 2, customer_id: @customer.id, tea_id: @tea.id }.to_json
+    
+    patch "/api/v1/customers/#{subscription_1.customer_id}/subscriptions/#{subscription_1.id}", params: subscription_params, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(response).to be_successful
+    expect(parsed[:title]).to eq("Subscription 1")
+    expect(parsed[:price]).to eq(5.00)
+    expect(parsed[:status]).to eq("active")
+    expect(parsed[:frequency]).to eq("2")
+    expect(parsed[:customer_id]).to eq(@customer.id)
+    expect(parsed[:tea_id]).to eq(@tea.id)
+  end
+  
+  it "can update status of a subscription for a customer" do
+    subscription_1 = Subscription.create!(title: "Subscription 1", price: 5.00, status: "active", frequency: 1, customer_id: @customer.id, tea_id: @tea.id)
+    subscription_2 = Subscription.create!(title: "Subscription 2", price: 5.00, status: "active", frequency: 1, customer_id: @customer.id, tea_id: @tea.id)
+
+    expect(subscription_1.status).to eq("active")
+    expect(subscription_2.status).to eq("active")
+
+    @subscription_params = { status: "cancelled" }.to_json
+    patch "/api/v1/customers/#{subscription_1.customer_id}/subscriptions/#{subscription_1.id}", params: @subscription_params, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(parsed[:status]).to eq("cancelled")
+    expect(subscription_2.status).to eq("active")
+  end
+
+  it "a customer cannot update the subscription title, price, or tea_id" do
+    subscription_1 = Subscription.create!(title: "Subscription 1", price: 5.00, status: "active", frequency: 1, customer_id: @customer.id, tea_id: @tea.id)
+
+    @subscription_params = { title: "Subscription 2", price: 10.00, tea_id: 2 }.to_json
+    patch "/api/v1/customers/#{subscription_1.customer_id}/subscriptions/#{subscription_1.id}", params: @subscription_params, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(parsed[:title]).to eq("Subscription 1")
+    expect(parsed[:price]).to eq(5.00)
+    expect(parsed[:tea_id]).to eq(@tea.id)
   end
 
   it "can delete a subscription for a customer" do
